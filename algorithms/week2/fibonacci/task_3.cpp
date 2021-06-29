393d2da07ee48a20ea6f73760f37a9f4bad6a266
renat <khatymovrenat@gmail.com>
2021-06-30 00:56:42 +0300

    fix

--- algorithms/week2/fibonacci/task_3.cpp
+++ algorithms/week2/fibonacci/task_3.cpp
@@ -13,56 +13,56 @@ public:
         if (n == 1)
             return 1;
 
-        int64_t size = n + 1;
+        int main_size = 6*m > 5000 ? 6*m: 5000;
 
-        uint64_t array[size];
-        uint64_t mod_array[size];
+        uint64_t array[main_size];
 
         array[0] = 0;
         array[1] = 1;
 
-        mod_array[0] = 0;
-        mod_array[1] = 1;
-
         uint period{0};
 
-        for (int64_t i = 2; i <= n; i++)
+        for (int64_t i = 2; i <= main_size; i++)
         {
-            array[i] = (array[i - 1] + array[i - 2]);
+            array[i] = (array[i-1] + array[i-2])%m;
 
-            mod_array[i] = array[i] % m;
-
-            period = define_period(mod_array, i);
+            period = define_period(array, i);
 
             if (period == 0)
                 continue;
 
-            cout <<"period:" << period << endl;
-            return mod_array[n%period];
+            return array[n%period];
         }
 
-        return 0;
+        int cur       {0};
+        int prev      {1};
+        int prev_prev {0};
+
+        for (int64_t i = 2; i <= main_size; i++)
+        {
+            cur = (prev + prev_prev);
+            prev_prev = prev;
+            prev = cur;
+        }
+
+        return cur%m;
     }
 
     static uint define_period(uint64_t array[], int64_t sz)
     {
-        uint period{0};
-
         uint min_period{2};
 
-        for (uint p = min_period; p < sz/2; p++)
-        {
+        for (uint p = min_period; p <= sz/2; p++)
             for (uint i = 0; i <= p; i++)
             {
                 if (array[i] != array[i + p + 1])
                     break;
 
                 if (i == p)
-                    return p + 1;
+                    return p+1;
             }
-        }
 
-        return period;
+        return 0;
     }
 };
 
@@ -70,8 +70,11 @@ int main(void) {
     int64_t n;
     int m;
 //    std::cin >> n >> m;
-    std::cout << Fibonacci::get_remainder(200, 10) << std::endl;
-//    std::cout << Fibonacci::get_remainder(12589, 369) << std::endl;
+    std::cout << Fibonacci::get_remainder(60282445765134413, 2263) << "?= 974" << std::endl;
+    std::cout << Fibonacci::get_remainder(1598753, 25897) << "?= 20305" << std::endl;
+    std::cout << Fibonacci::get_remainder(10, 4) << "?= 3" << std::endl;
+    std::cout << Fibonacci::get_remainder(200, 10) << "?= 5" << std::endl;
+    std::cout << Fibonacci::get_remainder(12589, 369) << "?= 89" << std::endl;
 
     return 0;
 }
