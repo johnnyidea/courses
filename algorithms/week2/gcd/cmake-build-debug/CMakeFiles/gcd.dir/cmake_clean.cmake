f9f4abe8c34a5e927c13ee5dca8cfbddb5ef88cf
renat <khatymovrenat@gmail.com>
2021-07-01 00:37:29 +0300

    feature. Added task for greatest common divisor

---
+++ algorithms/week2/gcd/cmake-build-debug/CMakeFiles/gcd.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gcd.dir/task_1.cpp.o"
+  "gcd"
+  "gcd.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gcd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ algorithms/week2/gcd/task_1.cpp
@@ -0,0 +1,40 @@
+/*
+ * По данным двум числам 1<= a,b <= 2*10^9 найдите их наибольший общий делитель.
+ * Sample Input 1:  18 35
+ * Sample Output 1: 1
+ * Sample Input 2: 14159572 63967072
+ * Sample Output 2: 4
+ */
+
+#include <cassert>
+#include <cstdint>
+#include <iostream>
+
+template <class Int>
+Int gcd(Int a, Int b) {
+    assert(a > 0 && b > 0);
+
+    //Realise via Euclid algorithm
+    if (a == 0)
+        return b;
+
+    if (b == 0)
+        return a;
+
+    while (a != 0 && b!= 0)
+        if (a >= b)
+            a %= b;
+        else
+            b %= a;
+
+    return a != 0 ? a : b;
+}
+
+int main(void) {
+    std::int32_t a, b;
+//    std::cin >> a >> b;
+    std::cout << gcd(18 , 35) << "?= 1"<< std::endl;
+
+    std::cout << gcd(14159572 , 63967072) << "?= 4"<< std::endl;
+    return 0;
+}
