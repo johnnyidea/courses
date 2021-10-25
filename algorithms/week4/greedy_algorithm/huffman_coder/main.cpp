a985c032e955ccfa86c3b50a947b2929b1e1c9f3
renat <khatymovrenat@gmail.com>
2021-10-25 20:26:15 +0300

    feature. Huffman coder completed

--- algorithms/week4/greedy_algorithm/huffman_coder/main.cpp
+++ algorithms/week4/greedy_algorithm/huffman_coder/main.cpp
@@ -2,6 +2,7 @@
 #include <queue>
 #include <list>
 #include <algorithm>
+#include <map>
 
 //=============================================================================
 
@@ -10,14 +11,18 @@ using namespace std;
 using FreqCharList = vector<pair<int, char>>;
 
 //=============================================================================
-FreqCharList calc_char_freq(string& str)
+FreqCharList calc_char_freq(string str)
 {
     FreqCharList res;
 
-    for (int i = str.size() - 1; i > 0; i--)
+    if (str.size() == 1)
+        return {{1, str[0]}};
+
+    int i = str.size() - 1;
+    while (!str.empty())
     {
-        if (i >= str.size())
-            continue;
+        while (i >= str.size())
+            i--;
 
         int counter{0};
         char c = str[i];
@@ -47,6 +52,7 @@ FreqCharList calc_char_freq(string& str)
         else
             res.insert(insert_pos, make_pair(counter, c));
 
+        i--;
     }
 
     return res;
@@ -61,21 +67,117 @@ public:
     HuffmanTree* left;
     HuffmanTree* right;
 
-    HuffmanTree(char c, int freq, HuffmanTree* left, HuffmanTree* right);
+    HuffmanTree(char c_, int freq_, HuffmanTree* left_ = nullptr, HuffmanTree* right_ = nullptr)
+    {
+        c = c_;
+        freq = freq_;
+        left = left_;
+        right = right_;
+    }
+
     ~HuffmanTree() = default;
 };
-
 //=============================================================================
 HuffmanTree* build_tree(const FreqCharList& freq_char_list)
 {
-    
-}
+    auto comp_freq = [&](HuffmanTree* left, HuffmanTree* right)
+    {
+        return left->freq > right->freq;
+    };
 
+    priority_queue<HuffmanTree*, vector<HuffmanTree*>, decltype(comp_freq)> three(comp_freq);
+
+    for (const auto& f_c: freq_char_list)
+    {
+        HuffmanTree* new_node = new HuffmanTree(f_c.second, f_c.first);
+        three.push(new_node);
+    }
+
+    HuffmanTree * root;
+
+    while (three.size() > 1)
+    {
+        auto* l = three.top();
+        three.pop();
+        auto* r = three.top();
+        three.pop();
+
+        auto* new_node = new HuffmanTree(' ', l->freq + r->freq, l, r);
+
+        three.push(new_node);
+    }
+
+    root = three.top();
+
+    return root;
+}
+//=============================================================================
+string char_code(": ");
+
+void assign_letter_to_code(HuffmanTree *huffman_tree, vector<string>& codes)
+{
+    if (huffman_tree)
+    {
+        if (huffman_tree->left)
+            char_code += "0";
+
+        assign_letter_to_code(huffman_tree->left, codes);
+
+        if (huffman_tree->right)
+            char_code += "1";
+
+        assign_letter_to_code(huffman_tree->right, codes);
+
+        if (!huffman_tree->left && !huffman_tree->right)
+        {
+            if (char_code.size() == 2)
+                char_code += "0";
+
+            char_code.insert(char_code.begin(), huffman_tree->c);
+            codes.push_back(char_code);
+            char_code.pop_back();
+            char_code.erase(0, 1);
+        }
+        if (huffman_tree->left && huffman_tree->right)
+            char_code.pop_back();
+    }
+}
+//=============================================================================
+void print_info(vector<string>& codes, const string& str)
+{
+    string code;
+    map<char, string> code_map;
+    for (const auto& letter_code: codes)
+        code_map.emplace(letter_code[0], letter_code.substr(letter_code.find(": ") + 2));
+
+    for (const auto& c:str)
+        code.append(code_map[c]);
+
+    cout << codes.size() << " " << code.size() << endl;
+
+    for (const auto& letter_code: codes)
+        cout << letter_code << endl;
+
+    cout << code << endl;
+}
+//=============================================================================
+
+//=============================================================================
 int main()
 {
-    string str("accepted");
+    string str;
+    cin >> str;
 
     auto evaluated_str = calc_char_freq(str);
 
+    auto* huffman_three = build_tree(evaluated_str);
+    vector<string> codes;
+    codes.reserve(evaluated_str.size());
+
+    assign_letter_to_code(huffman_three, codes);
+
+    print_info(codes, str);
+
     return 0;
 }
+//=============================================================================
